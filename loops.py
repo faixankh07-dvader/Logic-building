d755e68ed138d10fd1590a95cac12725321e9654
faixankh07-dvader <faixankh07@gmail.com>
2026-05-11 00:34:45 +0500

    reverse_words updated

--- loops.py
+++ loops.py
@@ -1,3 +1,4 @@
+"""
 # Print each character of a word
 
 # word = "Python"
@@ -11,7 +12,7 @@ print(result)
 
 # print words tha has a or n in them
 
-word1 = input("Enter list of words: ").split()
+word1 = input("Enter list of words: ").split()   #split() breaks the string into a list of words using spaces
 
 result1 = []
 for w in word1:
@@ -19,3 +20,25 @@ for w in word1:
         result1.append(w)
 
 print(result1)
+"""
+
+# reverse words in sentence
+string = input("Enter a string: ").split()
+
+sresult = []
+
+for s in reversed(string):
+    sresult.append(s)
+
+print(sresult)
+
+# reverse string
+
+string = input("Enter a string: ")
+
+result = []
+
+for char in reversed(string):
+    result.append(char)
+
+print("".join(result))  # join combine the words reversed in a string
--- reverse_words.py
+++ reverse_words.py
@@ -1,4 +1,26 @@
+# reverse words in sentence
+string = input("Enter a string: ").split()
+sresult = []
+
+for s in reversed(string):
+    sresult.append(s)
+
+print(sresult)
+
+# reverse string
+sentence = input("Enter a string: ")
+
+result = []
+
+for char in reversed(sentence):
+    result.append(char)
+
+print("".join(result))  # join combine the words reversed in a string
+
+
 # clean and reverse words
+
+
 def reverse_words(sentence):
     words = sentence.split()
 
